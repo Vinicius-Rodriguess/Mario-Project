484f79657729438a371f927ba0216c8c4471c676
Vinicius-Rodriguess <123024036+Vinicius-Rodriguess@users.noreply.github.com>
2023-01-21 21:51:00 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,22 @@
+const modal = document.querySelector(".modal");
+const botaoFecharModal = document.querySelector (".fechar-modal");
+const botaoTrailer = document.querySelector(".botao-trailer");
+const video = document.getElementById("video")
+const linkDoVideo = video.src;
+
+function alternarModal(){
+    modal.classList.toggle("aberto");
+}
+
+botaoTrailer.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", linkDoVideo);
+});
+
+botaoFecharModal.addEventListener("click", () => {
+    alternarModal();
+    video.setAttribute("src", "");
+});
+
+
+
